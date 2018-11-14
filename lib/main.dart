6a1226389545570b77edce5394fa7e2785b31605
akashMehta-STL <akash.mehta@solutelabs.com>
2018-11-14 15:24:58 +0530

    Create sample flutter ui.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(IntentApp());
 
-class MyApp extends StatelessWidget {
+class IntentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,6 +15,41 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
     );
   }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World"),
+        ),
+        body:SampleRactangle(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SampleRactangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Padding(padding: EdgeInsets.all(10.0), child: Text("Hi", textAlign: TextAlign.center, style: TextStyle(fontSize: 40.0),),),
+      ),
+    );
+  }
+
 }
 
 class SamplePage extends StatefulWidget {
