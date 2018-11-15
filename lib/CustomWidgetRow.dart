2a9596b06a2b5341424a24415c0f96a805d6436b
akashMehta-STL <akash.mehta@solutelabs.com>
2018-11-15 16:56:11 +0530

    Performed Quiz 13 : Quiz: Code Your Own Category Widget [ ref : https://classroom.udacity.com/courses/ud905/lessons/92a39eec-0c04-4d98-b47f-c884b9cd5a3b/concepts/434cfc1b-b9d4-4ff9-a723-38b4e5cfaae4]

--- lib/CustomWidgetRow.dart
+++ lib/CustomWidgetRow.dart
@@ -1,32 +1,49 @@
 import 'package:flutter/material.dart';
 
 class CustomWidgetRow extends StatelessWidget {
+  static const _borderRadius = BorderRadius.all(Radius.circular(10.0));
+  static const _rowHeight = 100.0;
+  static const _padding = 8.0;
+  static const _iconSize = 60.0;
+  static const _fontSize = 24.0;
+  static const _iconRightPadding = 16.0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Custom rows",
-      home: Scaffold(
-        body: Container(
-            child: InkWell(
-                highlightColor: Colors.amberAccent,
-                splashColor: Colors.greenAccent,
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 16.0),
-                        child: Icon(Icons.cake),
+    assert(debugCheckHasMaterial(context));
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+          child: InkWell(
+              borderRadius: _borderRadius,
+              highlightColor: Colors.amberAccent,
+              splashColor: Colors.greenAccent,
+              onTap: () {
+                print("Item is clicked");
+              },
+              child: Padding(
+                padding: EdgeInsets.all(_padding),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right: _iconRightPadding),
+                      child: Icon(Icons.cake,
+                        size: _iconSize,
                       ),
-                      Center(
-                        child: Text(
-                          "Hello",
-                          textAlign: TextAlign.center,
+                    ),
+                    Center(
+                      child: Text(
+                        "Hello",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: _fontSize
                         ),
-                      )
-                    ],
-                  ),
-                ))),
+                      ),
+                    )
+                  ],
+                ),
+              )
+          )
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 import 'CustomWidgetRow.dart';
 
-void main() => runApp(CustomWidgetRow());
+void main() => runApp(MyApp());
 
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+        body: Center(
+          child: CustomWidgetRow(),
+        )
+      ),
+    );
+  }
+
+}
