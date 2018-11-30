b95ad201f8a66ed1773288fbd1ac176ec4d8b1df
akashmehta <akashmehta4444@gmail.com>
2018-11-30 14:26:27 +0530

    Integrate https://jsonplaceholder.typicode.com/posts/{postId} using http.

--- lib/futureWidget/RemoteItem.dart
+++ lib/futureWidget/RemoteItem.dart
@@ -1,24 +1,56 @@
-
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/futureWidget/Post.dart';
 import 'package:http/http.dart';
 
 class RemoteItem extends StatelessWidget {
-  Future<Response> fetchResponse() {
-    return get('https://jsonplaceholder.typicode.com/posts/1');
+  Future<Post> fetchResponse() async {
+    final response = await get('https://jsonplaceholder.typicode.com/posts/1');
+
+    if (response.statusCode == 200) {
+      return Post.fromJson(JsonDecoder().convert(response.body));
+    } else {
+      throw Exception("Unable to load items");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-
-
-    return FutureBuilder(
-      future: post,
-      builder: (context, snapshot) {
-        return CircularProgressIndicator();
-      },
+    return Material(
+      child: Center(
+        child: FutureBuilder(
+          future: fetchResponse(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      snapshot.data.title,
+                      style: TextStyle(fontSize: 14, color: Colors.black),
+                    ),
+                    Text(
+                      snapshot.data.body,
+                      style: TextStyle(fontSize: 12, color: Colors.black),
+                    ),
+                  ],
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            return CircularProgressIndicator(
+              backgroundColor: Colors.white,
+            );
+          },
+        ),
+      ),
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/futureWidget/RemoteItem.dart';
 import 'package:flutter_app/sampleCounter/CounterWidget.dart';
 import 'package:flutter_app/unitConvertor/UnitList.dart';
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UnitList(),
+      home: RemoteItem(),
     );
   }
 
