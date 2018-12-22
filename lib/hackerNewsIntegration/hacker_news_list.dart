4711da6a8bd8924a1e0a6667f2307931216e23f0
akashmehta <akashmehta4444@gmail.com>
2018-12-22 16:35:08 +0530

    Resolved functional widget issue.

--- lib/hackerNewsIntegration/hacker_news_list.dart
+++ lib/hackerNewsIntegration/hacker_news_list.dart
@@ -5,12 +5,18 @@ import 'package:flutter_app/common/error_text_widget.dart';
 import 'package:flutter_app/hackerNewsIntegration/news_list_bloc.dart';
 import 'package:flutter_app/hackerNewsIntegration/news_list_item.dart';
 import 'package:flutter_app/hackerNewsIntegration/news_response.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+part 'hacker_news_list.g.dart';
+
+NewsListBloc newsListBloc;
+String contentType;
 
 class HackerNewsListWidget extends StatefulWidget {
-  final String type;
-  EventModel model;
-
-  HackerNewsListWidget(this.type) : assert(type != null);
+  HackerNewsListWidget(String type) :
+        assert(type != null){
+    contentType = type;
+  }
 
   @override
   _HackerNewsListWidgetState createState() => _HackerNewsListWidgetState();
@@ -18,7 +24,7 @@ class HackerNewsListWidget extends StatefulWidget {
 
 class _HackerNewsListWidgetState extends State<HackerNewsListWidget>
     with AutomaticKeepAliveClientMixin<HackerNewsListWidget> {
-  NewsListBloc newsListBloc;
+
   List<NewsResponseItem> itemList = List<NewsResponseItem>();
   bool isLoading = false;
 
@@ -26,66 +32,69 @@ class _HackerNewsListWidgetState extends State<HackerNewsListWidget>
   void initState() {
     super.initState();
     newsListBloc = BlocProvider.of<NewsListBloc>(context);
-    newsListBloc.fetchNewsItems(widget.type);
+    newsListBloc.fetchNewsItems(contentType);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: _createList(context, newsListBloc));
-  }
-
-  Widget _createList(BuildContext context, NewsListBloc newsListBloc) {
-    return StreamBuilder(
-        initialData: EventModel(true, null, null),
-        stream: newsListBloc.eventStream,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          widget.model = snapshot.data;
-          return _getPage(snapshot.data);
-        });
-  }
-
-  Widget _getPage(EventModel model) {
-    isLoading = false;
-    if (model.progress) {
-      if (itemList.isEmpty) {
-        return ProgressIndicatorWidget();
-      } else {
-        return _createListView(itemList);
-      }
-    } else if (model.response != null) {
-      if (itemList.contains(null)) {
-        itemList.remove(null);
-      }
-      itemList.addAll(model.response);
-      itemList.add(null);
-      return _createListView(itemList);
-    } else {
-      if (itemList.isEmpty) {
-        return ErrorTextWidget(model.error);
-      } else {
-        return _createListView(itemList);
-      }
-    }
-  }
-
-  Widget _createListView(List<NewsResponseItem> itemList) {
-    ScrollController _scrollController = ScrollController();
-    _scrollController.addListener(() {
-      if (_scrollController.position.maxScrollExtent ==
-          _scrollController.position.pixels) {
-        if (!isLoading) {
-          isLoading = !isLoading;
-          newsListBloc.fetchNewsItems(widget.type);
-        }
-      }
-    });
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: itemList.length,
-        itemBuilder: (BuildContext context, int index) =>
-            NewsListItem(itemList[index]));
+    return Container(child: CreateList(newsListBloc, itemList, isLoading, contentType));
   }
 
   @override
   bool get wantKeepAlive => true;
 }
+
+@widget
+Widget createList(BuildContext context, NewsListBloc newsListBloc, List<NewsResponseItem> itemList, bool isLoading, String type) {
+  return StreamBuilder(
+      initialData: EventModel(true, null, null),
+      stream: newsListBloc.eventStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return GetPage(snapshot.data, itemList, isLoading, type);
+      });
+}
+
+@widget
+Widget getPage(BuildContext context, EventModel model, List<NewsResponseItem> itemList, bool isLoading, String type) {
+  isLoading = false;
+  if (model.progress) {
+    if (itemList.isEmpty) {
+      return ProgressIndicatorWidget();
+    } else {
+      return CreateListView(itemList, isLoading, type);
+    }
+  } else if (model.response != null) {
+    if (itemList.contains(null)) {
+      itemList.remove(null);
+    }
+    itemList.addAll(model.response);
+    itemList.add(null);
+    return CreateListView(itemList, isLoading, type);
+  } else {
+    if (itemList.isEmpty) {
+      return ErrorTextWidget(model.error);
+    } else {
+      return CreateListView(itemList, isLoading, type);
+    }
+  }
+}
+
+@widget
+Widget createListView(BuildContext context, List<NewsResponseItem> itemList, bool isLoading, String type) {
+  ScrollController _scrollController = ScrollController();
+  _scrollController.addListener(() {
+    if (_scrollController.position.maxScrollExtent ==
+        _scrollController.position.pixels) {
+      if (!isLoading) {
+        isLoading = !isLoading;
+        newsListBloc.fetchNewsItems(type);
+      }
+    }
+  });
+  return ListView.builder(
+      controller: _scrollController,
+      itemCount: itemList.length,
+      itemBuilder: (BuildContext context, int index) =>
+          NewsListItem(itemList[index]));
+}
+
--- lib/hackerNewsIntegration/news_home.dart
+++ lib/hackerNewsIntegration/news_home.dart
@@ -11,23 +11,23 @@ class NewsHomeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(home: const NewsTabController());
   }
+}
 
-  @widget
-  Widget newsTabController(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Hacker News"),
-            centerTitle: true,
-            bottom: TabBar(
-                tabs: [Tab(text: "New"), Tab(text: "Top"), Tab(text: "Best")]),
-          ),
-          body: TabBarView(children: [
-            BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("New"),),
-            BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("Top"),),
-            BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("Best"))
-          ]),
-        ));
-  }
+@widget
+Widget newsTabController(BuildContext context) {
+  return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Hacker News"),
+          centerTitle: true,
+          bottom: TabBar(
+              tabs: [Tab(text: "New"), Tab(text: "Top"), Tab(text: "Best")]),
+        ),
+        body: TabBarView(children: [
+          BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("New"),),
+          BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("Top"),),
+          BlocProvider(bloc: NewsListBloc(), child: HackerNewsListWidget("Best"))
+        ]),
+      ));
 }
